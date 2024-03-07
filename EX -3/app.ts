553147a587fb6f9e61b37468e078641e4d2e826e
ALI HAMZA NADEEM RANA <160017611+hamzaa-ali@users.noreply.github.com>
2024-03-07 14:59:28 +0500

    Add files via upload

---
+++ EX -3/app.ts
@@ -0,0 +1,18 @@
+//lowercase
+let msg1 :string="HELLO EVERYONE"
+console.log(msg1.toLowerCase());
+
+//UPPERCASE
+let msg2:string="what are u doing know a dayss"
+let ToUppcasename:string=msg2.toUpperCase()
+console.log(ToUppcasename);
+
+//TileCase
+function toTitleCase(str: string): string {
+    return str.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
+}
+
+const inputString = "HELLO everyone";
+const titleCaseString = toTitleCase(inputString);
+console.log(titleCaseString);
+
